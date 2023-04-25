351d4289ecde3dfc3863d3de9dce809dcfd8511e
Karanbit01 <131760155+Karanbit01@users.noreply.github.com>
2023-04-25 15:49:17 +0530

    Remove some extended classes

--- android/src/main/kotlin/com/example/calender_event/CalenderEventPlugin.kt
+++ android/src/main/kotlin/com/example/calender_event/CalenderEventPlugin.kt
@@ -1,39 +1,41 @@
-package com.example.calender_event
+package com.mumble.flutter_is_awesome
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.ContactsContract
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import java.util.Calendar
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
-import kotlinx.android.synthetic.main.activity_main.*
+class FlutterIsAwesomePlugin: FlutterPlugin, MethodCallHandler {
 
-/** CalenderEventPlugin */
-class : FlutterPlugin, MethodCallHandler, FlutterActivity {
-  /// The MethodChannCalenderEventPluginel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+ 
+  var act: Activity? = null
   private lateinit var channel : MethodChannel
+  private lateinit var result: Result
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "add_calender_event")
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "add_Calander_Event")
     channel.setMethodCallHandler(this)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else if (call.method == "addCalanderEvent") {
-          var title : String = call.argument<String>("event_name")
-          val greetings = addCalanderEventCode("")
-          result.success(greetings)
+    this.result = result
+    if (call.method == "addCalanderEvent") {
+       val intent = Intent(Intent.ACTION_EDIT)
+    intent.setType("vnd.android.cursor.item/event")
+    intent.putExtra("beginTime", 10000)
+    intent.putExtra("endTime", 12000 + 60 * 60 * 1000)
+    intent.putExtra("title", "GS Wellness")
+      act?.startActivity(intent)
     } else {
       result.notImplemented()
     }
@@ -43,15 +45,5 @@ class : FlutterPlugin, MethodCallHandler, FlutterActivity {
     channel.setMethodCallHandler(null)
   }
 
-  private fun addCalanderEventCode(title: String){
-  val cal: Calendar = Calendar.getInstance()
-      val intent = Intent(Intent.ACTION_EDIT)
-      intent.setType("vnd.android.cursor.item/event")
-      intent.putExtra("beginTime", cal.getTimeInMillis())
-      intent.putExtra("endTime", cal.getTimeInMillis() + 60 * 60 * 1000)
-      intent.putExtra("title", title)
-      startActivity(intent)
-  }
-
+  
 }
-
